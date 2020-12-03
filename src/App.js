8fc5395077686ac628adcf88232701946c90033d
unknown <clgdevweb@gmail.com>
2020-12-04 00:30:42 +0100

    start app, config, player, data (wip)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './styles/app.scss';
+import data from './util';
+import Player from './components/Player';
+import Song from './components/Song';
 
 function App() {
+  //State
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+
   return (
     <div className="App">
-      <h1>Music Player</h1>
+      <Song currentSong={currentSong} />
+      <Player />
     </div>
   );
 }
